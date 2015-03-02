60652410c89f101268a4560a7ccf8f7bc0e20ff1
Matthew Rothenberg <mrothenberg@gmail.com>
2015-03-02 11:55:15 -0500

    grab git worktree root

--- commands/status/status.go
+++ commands/status/status.go
@@ -55,9 +55,35 @@ The output is prettier and more concise than standard 'git status'.
 }
 
 func runStatus() {
-	gitStatusOutput, err := exec.Command("git", "status", "--porcelain", "-b").Output()
+	gitProjectRoot() // := root
+	// root should be used to calculate absolute path which is what SHOULD BE the
+	// path for the FILE in statusItem.  From that we can calculate relative path
+	// for display in print, and either use abs or relative for fileList based
+	// on --RELATIVE flag!!!!!!!!!!!!!! <--- this should work
+
+	results := Process(gitStatusOutput())
+	results.printStatus(porcelainFiles)
+}
+
+// Runs `git status --porcelain` and returns the results.
+//
+// If an error is encountered, the process will die fatally.
+func gitStatusOutput() []byte {
+	gso, err := exec.Command("git", "status", "--porcelain", "-b").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return gso
+}
+
+// Returns the root for the git project.
+//
+// If can't be found, the process will die fatally.
+func gitProjectRoot() []byte {
+	gpr, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
 
 	if err != nil {
+		// we want to capture and handle status 128 in a pretty way
 		if err.Error() == "exit status 128" {
 			msg := "Not a git repository (or any of the parent directories)"
 			fmt.Fprintf(os.Stderr, "\033[0;31m"+msg+"\n")
@@ -66,7 +92,5 @@ func runStatus() {
 		// or, some other sort of error?
 		log.Fatal(err)
 	}
-
-	results := Process(gitStatusOutput)
-	results.printStatus(porcelainFiles)
+	return gpr
 }
