c3a32e7f6a5fe3a64a531c4f1032188fd72df2f5
Matthew Rothenberg <mrothenberg@gmail.com>
2015-03-02 18:53:00 -0500

    cleanup

--- commands/inits/data/status_shortcuts.sh
+++ commands/inits/data/status_shortcuts.sh
@@ -4,36 +4,30 @@ scmpuff_status_shortcuts() {
   # Ensure shwordsplit is on for zsh
   if [ -n "$ZSH_VERSION" ]; then setopt shwordsplit; fi;
 
-  scmpuff_clear_vars
-
   # Run scmpuff status, store output
-  # (`local` needs to be on its own line otherwise cmd exit code is swallowed!)
+  # (`local` needs to be on its own line otherwise exit code is swallowed!)
   local cmd_output
   cmd_output="$(/usr/bin/env scmpuff status --filelist $@)"
 
-  # if there was an error, exit prematurely
-  # we swallowed STDOUT but not STDERR, so user should see error status
+  # if there was an error, exit prematurely, and pass along the exit code
+  # (STDOUT was swallowed but not STDERR, so user should still see error msg)
   local es=$?
   if [ $es -ne 0 ]; then
     return $es
   fi
 
-  # Fetch list of files from first line of script output
+  # Fetch list of files (from first line of script output)
   files="$(echo "$cmd_output" | head -n 1)"
 
   # Export numbered env variables for each file
+  scmpuff_clear_vars
   IFS="|"
   local e=1
   for file in $files; do
     export $scmpuff_env_char$e="$file"
-    if [ "${SCMPUFF_DEBUG:-}" = "true" ]; then
-      echo "Set \$$scmpuff_env_char$e  => $file"
-    fi
     let e++
   done
   IFS=$' \t\n'
-
-  if [ "${SCMPUFF_DEBUG:-}" = "true" ]; then echo "------------------------"; fi
 
   # Print status (from line two onward)
   echo "$cmd_output" | tail -n +2
