7f1864c7810df12a3f8dcfac57beb7315d0f7964
Matthew Rothenberg <mrothenberg@gmail.com>
2015-03-02 15:58:44 -0500

    unit tests
    in order to test easier, moved the check for os.WorkDir up to the
    ProcessChange function, so it could be stubbed more easily

--- commands/status/process_test.go
+++ commands/status/process_test.go
@@ -5,65 +5,136 @@ import (
 	"testing"
 )
 
+// single test to make sure everything gets stiched together properly, test
+// actual cases in more specific methods
+func TestProcessChange(t *testing.T) {
+	chunk := []byte("A  TODO.md")
+	actual := ProcessChange(chunk, "/tmp")[0]
+
+	expectedChange := &change{
+		msg:   "  new file",
+		col:   neu,
+		group: Staged,
+	}
+	if actual.col != expectedChange.col ||
+		actual.group != expectedChange.group ||
+		actual.msg != expectedChange.msg {
+		t.Fatal("changes did not match expected")
+	}
+
+	if actual.fileAbsPath != "/tmp/TODO.md" {
+		t.Fatal("absolute path did not match expected")
+	}
+
+	if actual.fileRelPath == "" {
+		t.Fatal("relative path was not present")
+	}
+}
+
+var testCasesExtractFile = []struct {
+	root        string
+	wd          string
+	chunk       []byte
+	expectedAbs string
+	expectedRel string
+}{
+	{
+		root:        "/",
+		wd:          "/",
+		chunk:       []byte(" M script/benchmark"),
+		expectedAbs: "/script/benchmark",
+		expectedRel: "script/benchmark",
+	},
+	{
+		root:        "/tmp",
+		wd:          "/tmp",
+		chunk:       []byte(" M script/benchmark"),
+		expectedAbs: "/tmp/script/benchmark",
+		expectedRel: "script/benchmark",
+	},
+	{
+		root:        "/tmp/foo/bar//",
+		wd:          "/tmp/foo/bar/unicorn",
+		chunk:       []byte("?? unicorn/magic/xxx"),
+		expectedAbs: "/tmp/foo/bar/unicorn/magic/xxx",
+		expectedRel: "magic/xxx",
+	},
+	{
+		root:        "/tmp/foo/bar//",
+		wd:          "/tmp/foo/bar/unicorn/magic",
+		chunk:       []byte("?? narwhal/disco/yyy"),
+		expectedAbs: "/tmp/foo/bar/narwhal/disco/yyy",
+		expectedRel: "../../narwhal/disco/yyy",
+	},
+}
+
+func TestExtractFile(t *testing.T) {
+	for _, tc := range testCasesExtractFile {
+		actualAbs, actualRel := extractFile(tc.chunk, tc.root, tc.wd)
+
+		if actualAbs != tc.expectedAbs {
+			t.Fatalf(
+				"extractFile(%s)/absPath:\nexpect\t%v\nactual\t%v",
+				tc.chunk, tc.expectedAbs, actualAbs)
+		}
+		if actualRel != tc.expectedRel {
+			t.Fatalf(
+				"extractFile(%s)/relPath:\nexpect\t%v\nactual\t%v",
+				tc.chunk, tc.expectedRel, actualRel)
+		}
+	}
+}
+
 // $ git status --porcelain
 // A  TODO.md
 //  M script/benchmark
 // ?? .travis.yml
 // ?? commands/status/process_test.go
-
-var testCasesProcessChange = []struct {
-	arg      []byte
-	expected []*StatusItem
+var testCasesExtractChangeCodes = []struct {
+	chunk    []byte
+	expected []*change
 }{
 	{
 		[]byte("A  TODO.md"),
-		[]*StatusItem{
-			&StatusItem{msg: "  new file", col: neu, group: Staged, file: "TODO.md"},
+		[]*change{
+			&change{msg: "  new file", col: neu, group: Staged},
 		},
 	},
 	{
 		[]byte(" M script/benchmark"),
-		[]*StatusItem{
-			&StatusItem{msg: "  modified", col: mod, group: Unstaged, file: "script/benchmark"},
+		[]*change{
+			&change{msg: "  modified", col: mod, group: Unstaged},
 		},
 	},
 	{
 		[]byte("?? .travis.yml"),
-		[]*StatusItem{
-			&StatusItem{msg: " untracked", col: unt, group: Untracked, file: ".travis.yml"},
+		[]*change{
+			&change{msg: " untracked", col: unt, group: Untracked},
 		},
 	},
 	{
 		[]byte(" D deleted_file"),
-		[]*StatusItem{
-			&StatusItem{msg: "   deleted", col: del, group: Unstaged, file: "deleted_file"},
+		[]*change{
+			&change{msg: "   deleted", col: del, group: Unstaged},
 		},
 	},
 	{
 		[]byte("AM added_then_modified_file"),
-		[]*StatusItem{
-			&StatusItem{msg: "  new file", col: neu, group: Staged, file: "added_then_modified_file"},
-			&StatusItem{msg: "  modified", col: mod, group: Unstaged, file: "added_then_modified_file"},
+		[]*change{
+			&change{msg: "  new file", col: neu, group: Staged},
+			&change{msg: "  modified", col: mod, group: Unstaged},
 		},
 	},
 }
 
-func TestProcessChange(t *testing.T) {
-	for _, tc := range testCasesProcessChange {
-		actual := ProcessChange(tc.arg)
+func TestExtractChangeCodes(t *testing.T) {
+	for _, tc := range testCasesExtractChangeCodes {
+		actual := extractChangeCodes(tc.chunk)
 		if !reflect.DeepEqual(actual, tc.expected) {
-			t.Fatalf("processChange('%s'): expected %v, actual %v", tc.arg, tc.expected, actual)
+			t.Fatalf("processChange('%s'): expected %v, actual %v", tc.chunk, tc.expected, actual)
 		}
 	}
 }
-
-//
-// $ git status --porcelain -b
-// ## master...origin/master [ahead 1]
-// A  TODO.md
-//  M script/benchmark
-// ?? .travis.yml
-// ?? commands/status/process_test.go
 
 // Examples of stuff we will want to parse:
 //
@@ -71,8 +142,8 @@ func TestProcessChange(t *testing.T) {
 // 		## master
 // 		## master...origin/master
 // 		## master...origin/master [ahead 1]
-var testCasesProcessBranch = []struct {
-	arg      []byte
+var testCasesExtractBranch = []struct {
+	chunk    []byte
 	expected *BranchInfo
 }{
 	{
@@ -99,32 +170,17 @@ var testCasesProcessBranch = []struct {
 		[]byte("## upstream...upstream/master [behind 3]"),
 		&BranchInfo{name: "upstream", ahead: 0, behind: 3},
 	},
-	// TODO: test and handle compound up/down status
+	{
+		[]byte("## upstream...upstream/master [ahead 5, behind 3]"),
+		&BranchInfo{name: "upstream", ahead: 5, behind: 3},
+	},
 }
 
-func TestProcessBranch(t *testing.T) {
-	for _, tc := range testCasesProcessBranch {
-		actual := ProcessBranch(tc.arg)
+func TestExtractBranch(t *testing.T) {
+	for _, tc := range testCasesExtractBranch {
+		actual := extractBranch(tc.chunk)
 		if !reflect.DeepEqual(actual, tc.expected) {
-			t.Fatalf("processBranch('%s'): expected %v, actual %v", tc.arg, tc.expected, actual)
+			t.Fatalf("processBranch('%s'): expected %v, actual %v", tc.chunk, tc.expected, actual)
 		}
 	}
 }
-
-//
-// $ gs
-// # On branch: master  |  +1  |  [*] => $e*
-// #
-// ➤ Changes to be committed
-// #
-// #       new file: [1] TODO.md
-// #
-// ➤ Changes not staged for commit
-// #
-// #       modified: [2] script/benchmark
-// #
-// ➤ Untracked files
-// #
-// #      untracked: [3] .travis.yml
-// #      untracked: [4] commands/status/process_test.go
-// #
