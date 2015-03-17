6260c54a816d19886be737d506a179ed2f33032d
Matthew Rothenberg <mrothenberg@gmail.com>
2015-03-16 22:22:26 -0400

    dynamic build versioning
    now when built with build script, version reported by binary should
    reflect its distance from tagged version and commit SHA.
    
    versions built directly at tagged release SHOULD have normal build
    number (though don’t think goxc will care anyhow when switching to
    that).
    
    as part of this moved version command out of its own package, because
    while it was consistent, it was a waste of folders and obscured where
    it was set.

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/mroth/scmpuff/commands/expand"
 	"github.com/mroth/scmpuff/commands/inits"
 	"github.com/mroth/scmpuff/commands/status"
-	"github.com/mroth/scmpuff/commands/version"
 
 	"github.com/mroth/scmpuff/vendor/_nuts/github.com/spf13/cobra"
 )
+
+var NAME = "scmpuff"
+var VERSION = "0.?.? (not using buildscript)" // overriden via build scripts
 
 var puffCmd = &cobra.Command{
 	Use:   "scmpuff",
@@ -17,9 +21,18 @@ var puffCmd = &cobra.Command{
 If you are just getting started, try the intro!`,
 }
 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version number",
+	Long:  `All software has versions. This is ours.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(NAME, VERSION)
+	},
+}
+
 func main() {
 	puffCmd.AddCommand(introCmd)
-	puffCmd.AddCommand(version.VersionCmd)
+	puffCmd.AddCommand(versionCmd)
 	puffCmd.AddCommand(inits.CommandInit())
 	puffCmd.AddCommand(expand.CommandExpand())
 	puffCmd.AddCommand(status.CommandStatus())
