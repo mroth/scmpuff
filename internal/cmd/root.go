8db10792ff7971c8029da89c7886a95f3a2b922d
Matthew Rothenberg <mroth@mroth.xyz>
2025-07-27 17:58:34 -0400

    cleanup: remove hidden debug xerror command
    NOTE: when testing, if you want to force an error to test, an easy way
    is to run `scmpuff init --shell=unknown` to trigger an error.

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -50,20 +50,6 @@ If you are just getting started, try the intro!`,
 	rootCmd.AddCommand(inits.NewInitCmd())
 	rootCmd.AddCommand(status.NewStatusCmd())
 
-	// For now, add a command that always returns an error for testing purposes
-	errCmd := &cobra.Command{
-		Use:    "xerror",
-		Short:  "A command that always returns an error",
-		Long:   `This command is for testing error handling.`,
-		Args:   cobra.NoArgs,
-		Hidden: true, // hide from help output
-		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.SilenceUsage = true // silence usage-on-error after args processed
-			return fmt.Errorf("this is a test error")
-		},
-	}
-	rootCmd.AddCommand(errCmd)
-
 	return rootCmd
 }
 
@@ -81,6 +67,10 @@ func Execute(version goversion.Info) {
 		// desired. Note that this can be done in the command's RunE function
 		// after args are processed, in order to print usage only on arg parsing
 		// errors.
+		//
+		// NOTE: when testing, if you want to force an error to test this path,
+		// an easy way is to run `scmpuff init --shell=unknown` to trigger an
+		// error.
 		os.Exit(1)
 	}
 }
